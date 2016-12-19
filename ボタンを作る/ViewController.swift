64fa1a51cb3a711f028c81bb5ebe1fb948015f94
ikutahikaru <hikaruikuta@hanae-no-MacBook-Pro.local>
2016-12-19 17:18:39 +0900

    isEnableButton
    set isEnableButton

--- ボタンを作る/ViewController.swift
+++ ボタンを作る/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBAction func randomButton(_ sender: UIButton) {
+       
+        let num = arc4random_uniform(100)
+        
+        numberLabel.text = String(num)
+    
+        okButton.isEnabled = (num>=50)
+    
+    }
+    @IBOutlet weak var okButton: UIButton!
 
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
