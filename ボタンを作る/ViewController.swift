08e7509425c67423991deb437800088fd1183541
ikutahikaru <hikaruikuta@hanae-no-MacBook-Pro.local>
2016-12-19 17:02:19 +0900

    First

---
+++ ボタンを作る/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ボタンを作る
+//
+//  Created by Hikaru Ikuta on 2016/12/19.
+//  Copyright © 2016年 Hikaru Ikuta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
